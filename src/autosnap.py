6c26293809163bdbc429f819cab604289f52627e
Enming.Zhang <enming.zhang@umcloud.com>
2018-08-13 01:04:59 -0400

    autosnap: add autosnap main process
    Signed-off-by: Enming.Zhang <enming.zhang@umcloud.com>

---
+++ src/autosnap.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import sys
+import threading
+import queue
+import logging
+import time
+import signal
+from datetime import datetime
+
+from common.config import Config
+from flusher import Flusher
+from csvoper import CSVOper
+from cephoper.ceph_snapshot_op import CephSnaper
+from httpserver.snapshot_http_server import SnapshotHttpServer, SnapshotHttpServerHandler
+
+snapshot_http_server = None
+CONF_FILE = '/home/enming/autosnap/autosnap.conf'
+
+def stop_service(signum, frame):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    print("The autosnap service is stoped at {}".format(current_time))
+    global snapshot_http_server
+    snapshot_http_server.shutdown()
+    sys.exit(0)
+
+def main():
+    Config.Parse(CONF_FILE)
+
+    if not CSVOper.create():
+        sys.exit(-1)
+
+    global snapshot_http_server
+    snapshot_http_server = SnapshotHttpServer((Config.ip, Config.port), SnapshotHttpServerHandler)
+    server_thread = threading.Thread(target = snapshot_http_server.serve_forever)
+    server_thread.setDaemon(True)
+    server_thread.start()
+
+    signal.signal(signal.SIGINT, stop_service)
+
+    flush_start_time = datetime.utcnow()
+    snap_start_time = datetime.utcnow()
+    flush_thread_running = False
+    snapshot_thread_running = False
+    while True:
+        now = datetime.utcnow()
+        flush_time_interval = (now - flush_start_time).seconds
+        if flush_time_interval >= Config.flush_queue_interval:
+            flush_thread = Flusher()
+            flush_thread.start()
+            flush_thread_running = True
+            flush_start_time = now
+
+        if now.minute == 0 or (now - snap_start_time).seconds >= 3600:
+            snapshot_thread.start()
+            snapshot_thread_running = True
+            snap_start_time = now
+
+        if flush_thread_running:
+            flush_thread.join()
+            flush_thread_running = False
+
+        if snapshot_thread_running:
+            snapshot_thread.join()
+            snapshot_thread_running = False
+
+        time.sleep(5)
+
+if __name__ == '__main__':
+    main()
