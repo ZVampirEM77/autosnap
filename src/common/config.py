ca5bb3dceb25fa5ba4ba9dd982bae69ba3780a05
Enming.Zhang <enming.zhang@umcloud.com>
2018-08-13 00:50:33 -0400

    autosnap: fix the global config not work
    Signed-off-by: Enming.Zhang <enming.zhang@umcloud.com>

--- src/common/config.py
+++ src/common/config.py
@@ -1,3 +1,5 @@
+import sys
+
 if sys.version_info >= (3, 0):
     from configparser import ConfigParser
 else:
@@ -10,11 +12,11 @@ class Config(object):
     flush_queue_interval = 0
 
     @classmethod
-    def Parse(cls, config_file = '../autosnap.conf'):
+    def Parse(cls, config_file = '../../autosnap.conf'):
         autosnap_conf = ConfigParser()
-        autosnap_conf.read('../' + config_file)
-        ip = autosnap_conf.get('global', 'ip')
-        port = autosnap_conf.getint('global', 'port')
-        debug_interval = autosnap_conf.getint('global', 'debug_interval')
-        flush_queue_interval = autosnap_conf.getint('global', 'flush_queue_interval')
-        snapshot_prefix = autosnap_conf.get('global', 'snapshot_prefix')
+        autosnap_conf.read(config_file)
+        Config.ip = autosnap_conf.get('global', 'ip')
+        Config.port = autosnap_conf.getint('global', 'port')
+        Config.debug_interval = autosnap_conf.getint('global', 'debug_interval')
+        Config.flush_queue_interval = autosnap_conf.getint('global', 'flush_queue_interval')
+        Config.snapshot_prefix = autosnap_conf.get('global', 'snapshot_prefix')
