77b56113e095d36ca5503c02853d711b369d07c9
Enming Zhang <enming.zhang@umcloud.com>
2018-08-13 21:38:49 +0800

    autosnap: support to query the auto-snapshot status of the specified image
    Signed-off-by: Enming Zhang <enming.zhang@umcloud.com>

--- src/httpserver/snapshot_http_server.py
+++ src/httpserver/snapshot_http_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import re
 
 if sys.version_info >= (3, 0):
     from socketserver import ThreadingMixIn
@@ -9,6 +10,7 @@ else:
     from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
     from SocketServer import ThreadingMixIn
 
+from csvoper import CSVOper
 from common.shop_queue import ShopQueue
 
 
@@ -21,10 +23,26 @@ class SnapshotHttpServerHandler(BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.1'
     # for debug
     def do_GET(self):
+        enabled = False
+        if '/enable_query' in self.path:
+            match_obj = re.match('/enable_query\/(.*)\/(.*)', self.path)
+            req_pool = match_obj.group(1)
+            req_image = match_obj.group(2)
+            print("pool = {}, image = {}".format(req_pool, req_image))
+            current_enable_images = CSVOper.read()
+            for image in current_enable_images:
+                if image['image_name'] == req_image and image['pool_name'] == req_pool:
+                    enabled = True
         self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write("SnapshotHttpServerHandler::do_POST\n".encode('utf-8'))
+        if enabled:
+            self.send_header('Content-Length', '4')
+            self.end_headers()
+            self.wfile.write("True".encode('utf-8'))
+        else:
+            self.send_header('Content-Length', '5')
+            self.end_headers()
+            self.wfile.write("False".encode('utf-8'))
+
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
